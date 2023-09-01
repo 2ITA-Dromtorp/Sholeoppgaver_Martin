efc3bdc3b9875a4a8690a479d22d92cd53b4b067
Martin Andersen <martinaleksanderandersen@outlook.com>
2023-09-01 15:12:09 +0200

    Hello

---
+++ GrunleggendeProgramering/grunnleggende_programmering/src/Oppgave1.js
@@ -0,0 +1,47 @@
+
+
+function DeloppgaveA() {
+         
+    return (
+        <div className='deloppgave'>
+            <h1> DeloppgaveA</h1>
+            <p>Hva skriver du i javascript dersom du vil gi variabelen test verdien 8?
+Hvilken datatype er dette?
+ </p>
+            <p>let test = 8 </p>
+            
+
+
+        </div>
+    )
+
+}
+
+function DeloppgaveB() {
+
+return (
+<div className='deloppgave'>
+    <h1> DeloppgaveB </h1>
+    <p>Hva skriver du dersom du vil gi variabelen test verdien "testverdi"?
+Hvilken datatype er dette? </p>
+    <p>test = "testverdi"</p>
+
+
+
+</div>
+
+)
+
+
+}
+
+function Oppgave1() {
+
+return (
+    <h1> Dette er Oppgave 1 </h1>
+)
+
+}
+
+export default Oppgave1;
+
