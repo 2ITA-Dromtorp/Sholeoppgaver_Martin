783826e0bb30275a22c3e4ec604884892275bd26
Martin Andersen <martinaleksanderandersen@outlook.com>
2023-12-06 15:11:40 +0100

    h

---
+++ navbar/src/navbar.js
@@ -0,0 +1,24 @@
+// Navbar.js
+import React from 'react';
+import navbar from './navbar.css'
+
+
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Select</a></li>
+        <li><a href="/about">Update</a></li>
+        <li><a href="/contact">Insert</a></li>
+        <li><a href="/services">Delete</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+ 
+
