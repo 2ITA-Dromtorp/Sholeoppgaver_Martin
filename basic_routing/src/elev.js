a02faaacf15bbbc3bb1adcca86b4ba887bb81a99
Martin Andersen <martinaleksanderandersen@outlook.com>
2023-12-06 15:11:20 +0100

    hoo

--- basic_routing/src/elev.js
+++ basic_routing/src/elev.js
@@ -10,3 +10,23 @@ console.log(name);
         </div>
     )
 }
+
+
+<div className='middle'>
+        <div className='LeftMid'>
+          <div className='Norsk' onClick={() => navigate('/info/Grunnleggende datakunnskap')}>
+            Grunnleggende datakunnskap
+          </div>
+          <div className='Heimkunskap' onClick={() => navigate('/info/Kroppsøving')}>
+            Kroppsøving
+          </div>
+        </div>
+        <div className='RightMid'>
+          <div className='Norsk' onClick={() => navigate('/info/norsk')}>
+            Norsk
+          </div>
+          <div className='Heimkunskap' onClick={() => navigate('/info/Heimkunskap')}>
+            Heimkunskap
+          </div>
+        </div>
+      </div>
