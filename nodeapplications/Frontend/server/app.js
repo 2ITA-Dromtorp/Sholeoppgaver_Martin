32b226d88e22f2151a5f38bdab30addc66176009
Martin Andersen <martinaleksanderandersen@outlook.com>
2024-02-01 13:56:29 +0100

    g

--- nodeapplications/Frontend/server/app.js
+++ nodeapplications/Frontend/server/app.js
@@ -1,22 +1,22 @@
- let mysql = require('mysql');
+// let mysql = require('mysql');
 
- let con = mysql.createConnection({
-   host: "localhost",
-   user: "test",
-   password: "tMbP(QnzcSXQa(sN",
-   port: "3306",
-   database: "mlem"
- });
+// let con = mysql.createConnection({
+//   host: "localhost",
+//   user: "test",
+//   password: "tMbP(QnzcSXQa(sN",
+//   port: "3306",
+//   database: "mlem"
+// });
 
- con.connect(function(err) {
-   if (err) throw err;
-   console.log("Connected!");
-   let sql = "CREATE TABLE testx| (name VARCHAR(255), address VARCHAR(255))";
-  con.query(sql, function (err, result) {
-     if (err) throw err;
-   console.log("Result: " + result);
-   });
- });
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   let sql = "CREATE TABLE testx| (name VARCHAR(255), address VARCHAR(255))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Result: " + result);
+//   });
+// });
 
 
 
